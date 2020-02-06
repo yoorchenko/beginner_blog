3dfe266711ccc665ad1abd0e558e40c14f251730
Michael <you@example.com>
2020-02-06 21:31:02 +0200

    Added Create, Update, Delete fucntions

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
-from .views import BlogListView, DetailPostView
+from .views import BlogListView, DetailPostView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from django.urls import path
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name = 'post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name = 'post_edit'),
     path('post/<int:pk>/', DetailPostView.as_view(), name='post_detail'),  # new
+    path('post/new', BlogCreateView.as_view(), name = "post_new"),
     path('', BlogListView.as_view(), name='home'),
+
 ]
